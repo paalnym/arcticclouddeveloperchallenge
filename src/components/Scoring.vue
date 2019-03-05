2f8ede270f501499bfdddca15be94bbce192d023
Mikael Svenson <miksvenson@gmail.com>
2019-03-05 09:13:24 +0100

    Final score

--- src/components/Scoring.vue
+++ src/components/Scoring.vue
@@ -5,13 +5,7 @@
         <div class="col-md-12">
           <h3>ACDC 2019</h3>
           <div class="row">
-            <iframe
-              width="933"
-              height="700"
-              src="https://app.powerbi.com/view?r=eyJrIjoiYjY1MDhjMjQtMTYzNy00NjIzLTk2YmYtZDVkNDg5NTA4NWI3IiwidCI6ImQwYzRlZmE3LTJiYjgtNDUyYS04Y2IwLWE2YThkM2JiNTM0MiIsImMiOjh9"
-              frameborder="0"
-              allowfullscreen="true"
-            ></iframe>
+            <img src="../assets/acdc2019finalscore.png" style="width:100%">
           </div>
         </div>
       </div>
